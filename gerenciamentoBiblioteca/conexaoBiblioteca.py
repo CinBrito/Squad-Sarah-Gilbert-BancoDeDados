7fcb46b127be3408c2164eb7345905882f200ee0
Cíntia Brito <134309478+CinBrito@users.noreply.github.com>
2024-02-08 18:14:13 -0300

    Update conexaoBiblioteca.py
    Consertando campo status dentro de empréstimo.

--- gerenciamentoBiblioteca/conexaoBiblioteca.py
+++ gerenciamentoBiblioteca/conexaoBiblioteca.py
@@ -8,6 +8,8 @@ cursor = conexao.cursor()
 # cursor.execute('CREATE TABLE Editora(id INT PRIMARY KEY, nome VARCHAR(255));')
 # cursor.execute('CREATE TABLE Livro (id INT PRIMARY KEY, titulo VARCHAR(255), genero VARCHAR(50), id_Editora INT, status BOOLEAN, id_Autor INT, Exemplares INT, FOREIGN KEY (id_Autor) REFERENCES Autor(id), FOREIGN KEY (id_Editora) REFERENCES Editora(id));')
 # cursor.execute('CREATE TABLE Emprestimo (id INT PRIMARY KEY, id_Usuario INT, id_Livro INT, status VARCHAR(20), data_emprestimo DATE, data_devolucao DATE, renovacao INT, FOREIGN KEY (id_Usuario) REFERENCES Usuario(id), FOREIGN KEY (id_Livro) REFERENCES Livro(ID), FOREIGN KEY (status) REFERENCES Livro(status));')
+cursor.execute('DROP TABLE Emprestimo;')
+cursor.execute('CREATE TABLE Emprestimo (id INT PRIMARY KEY, id_Usuario INT, id_Livro INT, status BOOLEAN, data_emprestimo DATE, data_devolucao DATE, renovacao INT, FOREIGN KEY (id_Usuario) REFERENCES Usuario(id), FOREIGN KEY (id_Livro) REFERENCES Livro(ID), FOREIGN KEY (status) REFERENCES Livro(status));')
 
 
 conexao.commit()
