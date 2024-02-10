423509b2550f9a0daa01df7064ef7febe37c5bc2
Dayanaferrer <dayanaferreira2@gmail.com>
2024-02-10 14:48:17 -0300

    Ajustando a busca de livro por autor

--- gerenciamentoBiblioteca/consultasBiblioteca.py
+++ gerenciamentoBiblioteca/consultasBiblioteca.py
@@ -30,7 +30,8 @@ def buscarLivrosEmprestados():
 # Localizar os livros escritos por um autor específico
 def buscarLivroporAutor():
     listagem = conexao.execute('SELECT Livro.titulo FROM Livro INNER JOIN Autor ON Livro.id_Autor = Autor.id WHERE Autor.nome = "Robert Cecil Martin";')
-    pass
+    for livro in listagem:
+        print(livro)
 
 # Verificar o número de cópias disponíveis de um determinado livro
 def buscarExemplaresPorLivro():
